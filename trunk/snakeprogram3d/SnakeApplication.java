3e3f207d24dec90129d00fac8d1f11ccab1f2f63
mat_the_bruce@yahoo.com <mat_the_bruce@yahoo.com@fcba2a0c-78d2-6c3d-4ab9-01dac4d42e10>
2011-08-03 21:32:05 +0000

    Version numbers.

--- trunk/snakeprogram3d/SnakeApplication.java
+++ trunk/snakeprogram3d/SnakeApplication.java
@@ -20,7 +20,7 @@ public class SnakeApplication{
     
    public static int MAXLENGTH=1500;
     
-   final public static String VERSION = "0.8";
+   final public static String VERSION = "0.82";
 
    /**
     * From the command line.
