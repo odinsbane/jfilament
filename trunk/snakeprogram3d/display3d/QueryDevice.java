30c5c767434bf8158c754e5bfb8fd2f1604d6697
mat_the_bruce@yahoo.com <mat_the_bruce@yahoo.com@fcba2a0c-78d2-6c3d-4ab9-01dac4d42e10>
2012-03-06 18:02:28 +0000

    0.92 - When the SnakeBufferedImages checks for Java3D and device capabilities a generic exception is caught, the Message is displayed and the stack trace is printed. This is for debugging purposes and should be updated as more specific exceptions are found.

--- trunk/snakeprogram3d/display3d/QueryDevice.java
+++ trunk/snakeprogram3d/display3d/QueryDevice.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package snakeprogram3d.display3d;
 
 /**
@@ -39,7 +34,6 @@ public class QueryDevice {
     public int MAXW, MAXH, MAXD;
 
     public QueryDevice(){
-        
         GraphicsConfigTemplate3D template = new GraphicsConfigTemplate3D();
 
         /*
