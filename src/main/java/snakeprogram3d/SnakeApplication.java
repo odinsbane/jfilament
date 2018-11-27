e915c76f9fe6f6991236c11112436a580dc31996
m smith <matthew.smith3@crick.ac.uk>
2018-11-27 13:13:38 +0000

    Updated versions to development/snapshot values.

--- src/main/java/snakeprogram3d/SnakeApplication.java
+++ src/main/java/snakeprogram3d/SnakeApplication.java
@@ -20,7 +20,7 @@ public class SnakeApplication{
     
    public static int MAXLENGTH=1500;
     
-   final public static String VERSION = "1.1.4";
+   final public static String VERSION = "1.1.5-dev";
 
    /**
     * From the command line.
